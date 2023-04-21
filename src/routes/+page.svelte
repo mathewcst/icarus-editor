c87f606fd171f7b4db9ca7f45182961339023e3a
Matheus Costa <mathfernandesz@gmail.com>
2023-04-21 03:08:19 -0300

    Home message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,23 @@
 	<title>Icarus Tools</title>
 </svelte:head>
 
-<div class="page">
-	<h1>Hello fellow Prospectors!</h1>
+<div class="hero bg-base-200">
+	<div class="text-center hero-content">
+		<div class="max-w-md">
+			<h1 class="text-5xl font-bold">Hello fellow prospectors!</h1>
+			<p class="py-6">
+				This is just some tools that I wish I had when playing. I hope you find them useful!
+			</p>
+
+			<div class="flex flex-row items-center justify-center">
+				<a href="/calculator" class="btn btn-primary">Calculator</a>
+
+				<button class="btn btn-ghost btn-disabled">
+					<iconify-icon icon="material-symbols:lab-profile-outline" class="text-xl" />
+					Profile Editor
+					<div class="ml-2 badge badge-error badge-sm">Soon</div>
+				</button>
+			</div>
+		</div>
+	</div>
 </div>
