2cb0c3194e811b1c15e6642d7db18fb5d03026fe
Matheus Costa <mathfernandesz@gmail.com>
2023-04-21 02:39:06 -0300

    Functions to calculate totals

--- src/routes/calculator/components/GridItem.svelte
+++ src/routes/calculator/components/GridItem.svelte
@@ -11,11 +11,20 @@
 		waterConsumptionTimesQuantity,
 		waterGenerationTimesQuantity
 	} from '$/lib/items';
+	import { onMount } from 'svelte';
 
 	export let item: Grid;
 
 	let item_name: string;
 	let item_quantity: number;
+
+	let total_power_generated = 0;
+	let total_power_consumed = 0;
+	let total_water_generated = 0;
+	let total_water_consumed = 0;
+
+	let total_power_left = 0;
+	let total_water_left = 0;
 
 	// FORM HANDLERS
 	const handleDeleteGrid = () => {
@@ -30,11 +39,85 @@
 		};
 
 		grid.addItemToGrid(new_item, item.id);
+
+		item_name = '';
+		item_quantity = 0;
+
+		// Recalculate totals
+		calculateTotalPowerConsumption();
+		calculateTotalPowerGeneration();
+		calculateTotalWaterConsumption();
+		calculateTotalWaterGeneration();
 	};
 
 	const handleDeleteItem = (object: Item) => {
 		grid.removeItemFromGrid(object, item.id);
+
+		// Recalculate totals
+		calculateTotalPowerConsumption();
+		calculateTotalPowerGeneration();
+		calculateTotalWaterConsumption();
+		calculateTotalWaterGeneration();
 	};
+
+	const calculateTotalPowerGeneration = () => {
+		let total = 0;
+
+		item.items.forEach((object) => {
+			total += powerGenerationTimesQuantity(object);
+		});
+
+		total_power_generated = total;
+		total_power_left = total - total_power_consumed;
+
+		return total;
+	};
+
+	const calculateTotalPowerConsumption = () => {
+		let total = 0;
+
+		item.items.forEach((object) => {
+			total += powerConsumptionTimesQuantity(object);
+		});
+
+		total_power_consumed = total;
+		total_power_left = total_power_generated - total;
+
+		return total;
+	};
+
+	const calculateTotalWaterGeneration = () => {
+		let total = 0;
+
+		item.items.forEach((object) => {
+			total += waterGenerationTimesQuantity(object);
+		});
+
+		total_water_generated = total;
+		total_water_left = total - total_water_consumed;
+
+		return total;
+	};
+
+	const calculateTotalWaterConsumption = () => {
+		let total = 0;
+
+		item.items.forEach((object) => {
+			total += waterConsumptionTimesQuantity(object);
+		});
+
+		total_water_consumed = total;
+		total_water_left = total_water_generated - total;
+
+		return total;
+	};
+
+	onMount(() => {
+		calculateTotalPowerConsumption();
+		calculateTotalPowerGeneration();
+		calculateTotalWaterConsumption();
+		calculateTotalWaterGeneration();
+	});
 </script>
 
 <div class="flex flex-col items-center justify-start p-4 my-4 bg-base-300">
@@ -56,21 +139,18 @@
 			<!-- head -->
 			<thead>
 				<tr>
-					<th />
 					<th>Name</th>
-					<th>#</th>
+					<th>Quantity</th>
 					<th>Power Gen.</th>
 					<th>Power Con.</th>
 					<th>Water Gen.</th>
 					<th>Water Con.</th>
 					<th>Actions</th>
-					<th />
 				</tr>
 			</thead>
 			<tbody>
 				{#each item.items as object}
 					<tr>
-						<th />
 						<th>{object.name}</th>
 						<th>{object.quantity}</th>
 						<th>{powerGenerationTimesQuantity(object)}</th>
@@ -84,10 +164,9 @@
 					</tr>
 				{/each}
 				<tr>
-					<th />
 					<th>
 						<select class="w-full max-w-xs select select-bordered" bind:value={item_name}>
-							<option disabled selected>Select Item</option>
+							<option disabled selected value="">Select Item</option>
 							{#each sorted_items() as obj}
 								<option value={obj.name}>{obj.name}</option>
 							{/each}
@@ -111,23 +190,48 @@
 					</th>
 				</tr>
 				<tr class="active">
-					<th />
 					<th>
 						<p>Totals</p>
 					</th>
 					<th />
 					<th>
-						<p>{powerGenerationTimesQuantity(item)}</p>
+						<p>{total_power_generated}</p>
 					</th>
 					<th>
-						<p>{powerConsumptionTimesQuantity(item)}</p>
+						<p>{total_power_consumed}</p>
 					</th>
 					<th>
-						<p>{waterGenerationTimesQuantity(item)}</p>
+						<p>{total_water_generated}</p>
 					</th>
 					<th>
-						<p>{waterConsumptionTimesQuantity(item)}</p>
+						<p>{total_water_consumed}</p>
 					</th>
+					<th />
+				</tr>
+				<tr>
+					<th>
+						<p>Power Left</p>
+					</th>
+					<th />
+					<th>
+						<p class:text-red-500={total_power_left < 0}>{total_power_left}</p>
+					</th>
+					<th />
+					<th />
+					<th />
+					<th />
+				</tr>
+				<tr>
+					<th>
+						<p>Water Left</p>
+					</th>
+					<th />
+					<th>
+						<p class:text-red-500={total_water_left < 0}>{total_water_left}</p>
+					</th>
+					<th />
+					<th />
+					<th />
 					<th />
 				</tr>
 			</tbody>
